77c4c5dfe12d784ef748316d5366695132bac939
farhanhossien <farhanhossien@users.noreply.github.com>
2026-06-08 20:34:16 +0600

    Change base path to relative in vite.config.ts for Vercel and Pages compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Farhan-TV/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
